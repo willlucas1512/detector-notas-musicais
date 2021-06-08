9b56532cfa6c3f39043adee2278a1d733c4dbfc7
Nayara Gomes (VPN) <nayara.gomes@venhapranuvem.com.br>
2021-06-07 22:01:42 -0300

    Exibindo frequência da onda sonora, ativando stop da detecção de som, alterando visual da frequência sonora e modificando função que controla estilo dos botões ao ativar/desativar microfone

--- functions.js
+++ functions.js
@@ -7,6 +7,8 @@ function controlMicrophoneButton() {
   controll++;
 
   if (controll % 2 == 0) {
+    noteElem.innerHTML = '--';
+    pitchElem.innerText = "--";
     micBtn.style.border = "none";
     micIcon.style.color = "#555555";
     infoTxt.innerHTML = "Clique no microfone para detectar";
@@ -87,7 +89,7 @@ window.onload = function () {
 }
 
 function error() {
-  alert('Stream generation failed.');
+  alert('Falha no Stream');
 }
 
 function getUserMedia(dictionary, callback) {
@@ -113,47 +115,30 @@ function gotStream(stream) {
   controlMicrophoneButton();
   updatePitch();
 }
-
-function toggleOscillator() {
-  if (isPlaying) {
-    //stop playing and return
-    sourceNode.stop(0);
-    sourceNode = null;
-    analyser = null;
-    isPlaying = false;
-    if (!window.cancelAnimationFrame)
-      window.cancelAnimationFrame = window.webkitCancelAnimationFrame;
-    window.cancelAnimationFrame(rafID);
-    return "play oscillator";
-  }
-  sourceNode = audioContext.createOscillator();
-
-  analyser = audioContext.createAnalyser();
-  analyser.fftSize = 2048;
-  sourceNode.connect(analyser);
-  analyser.connect(audioContext.destination);
-  sourceNode.start(0);
-  isPlaying = true;
-  isLiveInput = false;
-  updatePitch();
-
-  return "stop";
+function getLocalStream() {
+  navigator.mediaDevices.getUserMedia({video: false, audio: false}).then( stream => {
+      window.localStream = stream;
+      window.localAudio.srcObject = stream;
+      window.localAudio.autoplay = false;
+  }).catch( err => {
+      console.log( 'Erro:', err)
+  });
 }
 
 function toggleLiveInput() {
   if (isPlaying) {
     //stop playing and return
-    sourceNode.stop(0);
+    getLocalStream();
     sourceNode = null;
     analyser = null;
     isPlaying = false;
     if (!window.cancelAnimationFrame)
       window.cancelAnimationFrame = window.webkitCancelAnimationFrame;
     window.cancelAnimationFrame(rafID);
-    return "";
+    controlMicrophoneButton();
   } 
-  
-  getUserMedia(
+  else {
+    getUserMedia(
     {
       "audio": {
         "mandatory": {
@@ -166,35 +151,9 @@ function toggleLiveInput() {
       },
     }, gotStream
   );
-}
-
-function togglePlayback() {
-  if (isPlaying) {
-    //stop playing and return
-    sourceNode.stop(0);
-    sourceNode = null;
-    analyser = null;
-    isPlaying = false;
-    if (!window.cancelAnimationFrame)
-      window.cancelAnimationFrame = window.webkitCancelAnimationFrame;
-    window.cancelAnimationFrame(rafID);
-    return "start";
-  }
-
-  sourceNode = audioContext.createBufferSource();
-  sourceNode.buffer = theBuffer;
-  sourceNode.loop = true;
-
-  analyser = audioContext.createAnalyser();
-  analyser.fftSize = 2048;
-  sourceNode.connect(analyser);
-  analyser.connect(audioContext.destination);
-  sourceNode.start(0);
   isPlaying = true;
-  isLiveInput = false;
-  updatePitch();
-
-  return "stop";
+  }
+  
 }
 
 var rafID = null;
@@ -216,67 +175,6 @@ function frequencyFromNoteNumber(note) {
 function centsOffFromPitch(frequency, note) {
   return Math.floor(1200 * Math.log(frequency / frequencyFromNoteNumber(note)) / Math.log(2));
 }
-
-// this is the previously used pitch detection algorithm.
-/*
-var MIN_SAMPLES = 0;  // will be initialized when AudioContext is created.
-var GOOD_ENOUGH_CORRELATION = 0.9; // this is the "bar" for how close a correlation needs to be
-
-function autoCorrelate( buf, sampleRate ) {
-  var SIZE = buf.length;
-  var MAX_SAMPLES = Math.floor(SIZE/2);
-  var best_offset = -1;
-  var best_correlation = 0;
-  var rms = 0;
-  var foundGoodCorrelation = false;
-  var correlations = new Array(MAX_SAMPLES);
-
-  for (var i=0;i<SIZE;i++) {
-    var val = buf[i];
-    rms += val*val;
-  }
-  rms = Math.sqrt(rms/SIZE);
-  if (rms<0.01) // not enough signal
-    return -1;
-
-  var lastCorrelation=1;
-  for (var offset = MIN_SAMPLES; offset < MAX_SAMPLES; offset++) {
-    var correlation = 0;
-
-    for (var i=0; i<MAX_SAMPLES; i++) {
-      correlation += Math.abs((buf[i])-(buf[i+offset]));
-    }
-    correlation = 1 - (correlation/MAX_SAMPLES);
-    correlations[offset] = correlation; // store it, for the tweaking we need to do below.
-    if ((correlation>GOOD_ENOUGH_CORRELATION) && (correlation > lastCorrelation)) {
-      foundGoodCorrelation = true;
-      if (correlation > best_correlation) {
-        best_correlation = correlation;
-        best_offset = offset;
-      }
-    } else if (foundGoodCorrelation) {
-      // short-circuit - we found a good correlation, then a bad one, so we'd just be seeing copies from here.
-      // Now we need to tweak the offset - by interpolating between the values to the left and right of the
-      // best offset, and shifting it a bit.  This is complex, and HACKY in this code (happy to take PRs!) -
-      // we need to do a curve fit on correlations[] around best_offset in order to better determine precise
-      // (anti-aliased) offset.
-
-      // we know best_offset >=1, 
-      // since foundGoodCorrelation cannot go to true until the second pass (offset=1), and 
-      // we can't drop into this clause until the following pass (else if).
-      var shift = (correlations[best_offset+1] - correlations[best_offset-1])/correlations[best_offset];  
-      return sampleRate/(best_offset+(8*shift));
-    }
-    lastCorrelation = correlation;
-  }
-  if (best_correlation > 0.01) {
-    // console.log("f = " + sampleRate/best_offset + "Hz (rms: " + rms + " confidence: " + best_correlation + ")")
-    return sampleRate/best_offset;
-  }
-  return -1;
-//	var best_frequency = sampleRate/best_offset;
-}
-*/
 
 function autoCorrelate(buf, sampleRate) {
   // Implements the ACF2+ algorithm
