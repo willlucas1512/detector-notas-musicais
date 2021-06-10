82a174512b929ea20747c155d4f07d85f5d21267
willlucas1512 <41302062+willlucas1512@users.noreply.github.com>
2021-06-10 12:53:30 -0300

    resolve no chrome esperançosamente

--- functions.js
+++ functions.js
@@ -1,13 +1,13 @@
-var controll = 0
-var micBtn = document.querySelector('#microphone-icon-container')
-var micIcon = document.querySelector('#microphone-icon')
-var infoTxt = document.querySelector('#info-container span')
+var controll = 0;
+var micBtn = document.querySelector("#microphone-icon-container");
+var micIcon = document.querySelector("#microphone-icon");
+var infoTxt = document.querySelector("#info-container span");
 
 function controlMicrophoneButton() {
   controll++;
 
   if (controll % 2 == 0) {
-    noteElem.innerHTML = '--';
+    noteElem.innerHTML = "--";
     pitchElem.innerText = "--";
     micBtn.style.border = "none";
     micIcon.style.color = "#555555";
@@ -26,11 +26,11 @@ var isPlaying = false;
 var sourceNode = null;
 var analyser = null;
 var theBuffer = null;
-var DEBUGCANVAS = null;
+// var DEBUGCANVAS = null;
 var mediaStreamSource = null;
 var detectorElem,
   canvasElem,
-  waveCanvas,
+  // waveCanvas,
   pitchElem,
   noteElem,
   detuneElem,
@@ -38,25 +38,25 @@ var detectorElem,
 
 window.onload = function () {
   audioContext = new AudioContext();
-  MAX_SIZE = Math.max(4, Math.floor(audioContext.sampleRate / 5000));	// corresponds to a 5kHz signal
+  MAX_SIZE = Math.max(4, Math.floor(audioContext.sampleRate / 5000)); // corresponds to a 5kHz signal
   var request = new XMLHttpRequest();
-  request.open("GET", "../sounds/whistling3.ogg", true);
+  // request.open("GET", "../sounds/whistling3.ogg", true);
   request.responseType = "arraybuffer";
   request.onload = function () {
     audioContext.decodeAudioData(request.response, function (buffer) {
       theBuffer = buffer;
     });
-  }
+  };
   request.send();
 
   detectorElem = document.getElementById("detector");
   canvasElem = document.getElementById("output");
-  DEBUGCANVAS = document.getElementById("waveform");
-  if (DEBUGCANVAS) {
-    waveCanvas = DEBUGCANVAS.getContext("2d");
-    waveCanvas.strokeStyle = "black";
-    waveCanvas.lineWidth = 1;
-  }
+  // DEBUGCANVAS = document.getElementById("waveform");
+  // if (DEBUGCANVAS) {
+  // waveCanvas = DEBUGCANVAS.getContext("2d");
+  // waveCanvas.strokeStyle = "black";
+  // waveCanvas.lineWidth = 1;
+  // }
   pitchElem = document.getElementById("pitch");
   noteElem = document.getElementById("note");
   detuneElem = document.getElementById("detune");
@@ -66,7 +66,10 @@ window.onload = function () {
     this.classList.add("droptarget");
     return false;
   };
-  detectorElem.ondragleave = function () { this.classList.remove("droptarget"); return false; };
+  detectorElem.ondragleave = function () {
+    this.classList.remove("droptarget");
+    return false;
+  };
   detectorElem.ondrop = function (e) {
     this.classList.remove("droptarget");
     e.preventDefault();
@@ -74,10 +77,15 @@ window.onload = function () {
 
     var reader = new FileReader();
     reader.onload = function (event) {
-      audioContext.decodeAudioData(event.target.result, function (buffer) {
-        theBuffer = buffer;
-      }, function () { alert("error loading!"); });
-
+      audioContext.decodeAudioData(
+        event.target.result,
+        function (buffer) {
+          theBuffer = buffer;
+        },
+        function () {
+          alert("error loading!");
+        }
+      );
     };
     reader.onerror = function (event) {
       alert("Error: " + reader.error);
@@ -85,11 +93,10 @@ window.onload = function () {
     reader.readAsArrayBuffer(e.dataTransfer.files[0]);
     return false;
   };
-
-}
+};
 
 function error() {
-  alert('Falha no Stream');
+  alert("Falha no Stream");
 }
 
 function getUserMedia(dictionary, callback) {
@@ -100,7 +107,7 @@ function getUserMedia(dictionary, callback) {
       navigator.mozGetUserMedia;
     navigator.getUserMedia(dictionary, callback, error);
   } catch (e) {
-    alert('getUserMedia threw exception :' + e);
+    alert("getUserMedia threw exception :" + e);
   }
 }
 
@@ -116,18 +123,20 @@ function gotStream(stream) {
   updatePitch();
 }
 function getLocalStream() {
-  navigator.mediaDevices.getUserMedia({video: false, audio: false}).then( stream => {
+  navigator.mediaDevices
+    .getUserMedia({ video: false, audio: false })
+    .then((stream) => {
       window.localStream = stream;
       window.localAudio.srcObject = stream;
       window.localAudio.autoplay = false;
-  }).catch( err => {
-      console.log( 'Erro:', err)
-  });
+    })
+    .catch((err) => {
+      console.log("Erro:", err);
+    });
 }
 
 function toggleLiveInput() {
   if (isPlaying) {
-    //stop playing and return
     getLocalStream();
     sourceNode = null;
     analyser = null;
@@ -136,24 +145,23 @@ function toggleLiveInput() {
       window.cancelAnimationFrame = window.webkitCancelAnimationFrame;
     window.cancelAnimationFrame(rafID);
     controlMicrophoneButton();
-  } 
-  else {
+  } else {
     getUserMedia(
-    {
-      "audio": {
-        "mandatory": {
-          "googEchoCancellation": "false",
-          "googAutoGainControl": "false",
-          "googNoiseSuppression": "false",
-          "googHighpassFilter": "false"
+      {
+        audio: {
+          mandatory: {
+            googEchoCancellation: "false",
+            googAutoGainControl: "false",
+            googNoiseSuppression: "false",
+            googHighpassFilter: "false",
+          },
+          optional: [],
         },
-        "optional": []
       },
-    }, gotStream
-  );
-  isPlaying = true;
+      gotStream
+    );
+    isPlaying = true;
   }
-  
 }
 
 var rafID = null;
@@ -161,7 +169,20 @@ var tracks = null;
 var buflen = 2048;
 var buf = new Float32Array(buflen);
 
-var noteStrings = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+var noteStrings = [
+  "C",
+  "C#",
+  "D",
+  "D#",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "G#",
+  "A",
+  "A#",
+  "B",
+];
 
 function noteFromPitch(frequency) {
   var noteNum = 12 * (Math.log(frequency / 440) / Math.log(2));
@@ -173,7 +194,9 @@ function frequencyFromNoteNumber(note) {
 }
 
 function centsOffFromPitch(frequency, note) {
-  return Math.floor(1200 * Math.log(frequency / frequencyFromNoteNumber(note)) / Math.log(2));
+  return Math.floor(
+    (1200 * Math.log(frequency / frequencyFromNoteNumber(note))) / Math.log(2)
+  );
 }
 
 function autoCorrelate(buf, sampleRate) {
@@ -186,25 +209,35 @@ function autoCorrelate(buf, sampleRate) {
     rms += val * val;
   }
   rms = Math.sqrt(rms / SIZE);
-  if (rms < 0.01) // not enough signal
+  if (rms < 0.01)
+    // not enough signal
     return -1;
 
-  var r1 = 0, r2 = SIZE - 1, thres = 0.2;
+  var r1 = 0,
+    r2 = SIZE - 1,
+    thres = 0.2;
   for (var i = 0; i < SIZE / 2; i++)
-    if (Math.abs(buf[i]) < thres) { r1 = i; break; }
+    if (Math.abs(buf[i]) < thres) {
+      r1 = i;
+      break;
+    }
   for (var i = 1; i < SIZE / 2; i++)
-    if (Math.abs(buf[SIZE - i]) < thres) { r2 = SIZE - i; break; }
+    if (Math.abs(buf[SIZE - i]) < thres) {
+      r2 = SIZE - i;
+      break;
+    }
 
   buf = buf.slice(r1, r2);
   SIZE = buf.length;
 
   var c = new Array(SIZE).fill(0);
   for (var i = 0; i < SIZE; i++)
-    for (var j = 0; j < SIZE - i; j++)
-      c[i] = c[i] + buf[j] * buf[j + i];
+    for (var j = 0; j < SIZE - i; j++) c[i] = c[i] + buf[j] * buf[j + i];
 
-  var d = 0; while (c[d] > c[d + 1]) d++;
-  var maxval = -1, maxpos = -1;
+  var d = 0;
+  while (c[d] > c[d + 1]) d++;
+  var maxval = -1,
+    maxpos = -1;
   for (var i = d; i < SIZE; i++) {
     if (c[i] > maxval) {
       maxval = c[i];
@@ -213,7 +246,9 @@ function autoCorrelate(buf, sampleRate) {
   }
   var T0 = maxpos;
 
-  var x1 = c[T0 - 1], x2 = c[T0], x3 = c[T0 + 1];
+  var x1 = c[T0 - 1],
+    x2 = c[T0],
+    x3 = c[T0 + 1];
   a = (x1 + x3 - 2 * x2) / 2;
   b = (x3 - x1) / 2;
   if (a) T0 = T0 - b / (2 * a);
@@ -222,34 +257,33 @@ function autoCorrelate(buf, sampleRate) {
 }
 
 function updatePitch(time) {
-  var cycles = new Array;
+  var cycles = new Array();
   analyser.getFloatTimeDomainData(buf);
   var ac = autoCorrelate(buf, audioContext.sampleRate);
-  // TODO: Paint confidence meter on canvasElem here.
 
-  if (DEBUGCANVAS) {  // This draws the current waveform, useful for debugging
-    waveCanvas.clearRect(0, 0, 512, 256);
-    waveCanvas.strokeStyle = "red";
-    waveCanvas.beginPath();
-    waveCanvas.moveTo(0, 0);
-    waveCanvas.lineTo(0, 256);
-    waveCanvas.moveTo(128, 0);
-    waveCanvas.lineTo(128, 256);
-    waveCanvas.moveTo(256, 0);
-    waveCanvas.lineTo(256, 256);
-    waveCanvas.moveTo(384, 0);
-    waveCanvas.lineTo(384, 256);
-    waveCanvas.moveTo(512, 0);
-    waveCanvas.lineTo(512, 256);
-    waveCanvas.stroke();
-    waveCanvas.strokeStyle = "black";
-    waveCanvas.beginPath();
-    waveCanvas.moveTo(0, buf[0]);
-    for (var i = 1; i < 512; i++) {
-      waveCanvas.lineTo(i, 128 + (buf[i] * 128));
-    }
-    waveCanvas.stroke();
-  }
+  // if (DEBUGCANVAS) {
+  // waveCanvas.clearRect(0, 0, 512, 256);
+  // waveCanvas.strokeStyle = "red";
+  // waveCanvas.beginPath();
+  // waveCanvas.moveTo(0, 0);
+  // waveCanvas.lineTo(0, 256);
+  // waveCanvas.moveTo(128, 0);
+  // waveCanvas.lineTo(128, 256);
+  // waveCanvas.moveTo(256, 0);
+  // waveCanvas.lineTo(256, 256);
+  // waveCanvas.moveTo(384, 0);
+  // waveCanvas.lineTo(384, 256);
+  // waveCanvas.moveTo(512, 0);
+  // waveCanvas.lineTo(512, 256);
+  // waveCanvas.stroke();
+  // waveCanvas.strokeStyle = "black";
+  // waveCanvas.beginPath();
+  // waveCanvas.moveTo(0, buf[0]);
+  // for (var i = 1; i < 512; i++) {
+  //   waveCanvas.lineTo(i, 128 + buf[i] * 128);
+  // }
+  // waveCanvas.stroke();
+  // }
 
   if (ac == -1) {
     detectorElem.className = "vague";
@@ -268,10 +302,8 @@ function updatePitch(time) {
       detuneElem.className = "";
       detuneAmount.innerHTML = "--";
     } else {
-      if (detune < 0)
-        detuneElem.className = "flat";
-      else
-        detuneElem.className = "sharp";
+      if (detune < 0) detuneElem.className = "flat";
+      else detuneElem.className = "sharp";
       detuneAmount.innerHTML = Math.abs(detune);
     }
   }
